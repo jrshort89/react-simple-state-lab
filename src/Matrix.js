ed83a827626bcab9498138d6d21585a11ad31631
jrshort89 <jrshort89@gmail.com>
2020-12-09 18:50:06 -0500

    done

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React,{ Component } from 'react';
+import Cell from './Cell';
 
 export default class Matrix extends Component {
-  
+
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    return vals.map((val,index) => <Cell value={val} key={index} />) // replace me and render a cell component instead!
   }
-  
+
   genMatrix = () => {
-    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+    return this.props.values.map((rowVals,index) => <div className="row" key={index}>{this.genRow(rowVals)}</div>)
   }
-  
+
   render() {
     return (
       <div id="matrix">
@@ -17,5 +18,20 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
+
+}
+
+Matrix.defaultProps = {
+  values: [
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F'],
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F'],
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F'],
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F'],
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F'],
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F'],
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F'],
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F'],
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F'],
+    ['#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F','#F00','#00F']
+  ]
 }
